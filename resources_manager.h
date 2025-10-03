1cec0da5bff7216998fbcc5b34e1649c3b04d464
Mr_Taki <2028566723@qq.com>
2025-10-03 23:41:17 +0800

    src: Resources Manager

---
+++ resources_manager.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "atlas.h"
+#include "util.h"
+
+#include <string>
+#include <graphics.h>
+#include <unordered_map>
+
+class ResourcesManager
+{
+public:
+	// instance()方法用以返回resourcesManager唯一的实例
+	static ResourcesManager* instance();
+
+	void load();
+
+	Atlas* find_atlas(const std::string& id) const;
+	IMAGE* find_image(const std::string& id) const;
+
+private:
+	static ResourcesManager* manager;
+
+	std::unordered_map<std::string, Atlas*> atlas_pool;
+	std::unordered_map<std::string, IMAGE*> image_pool;
+	
+private:
+	ResourcesManager();
+	~ResourcesManager();
+
+	void flip_image(IMAGE* src_image, IMAGE* dst_image, int num_h = 1);
+	void flip_image(const std::string& src_id, const std::string& dst_id, int num_h = 1);
+	void flip_atlas(const std::string& src_id, const std::string& dst_id);
+};
+
