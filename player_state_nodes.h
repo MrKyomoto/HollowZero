af1c743ceccd932646ff9b62a2b67c8887d1218d
Mr_Taki <2028566723@qq.com>
2025-10-16 16:24:46 +0800

    src: FSM player node

---
+++ player_state_nodes.h
@@ -0,0 +1,96 @@
+#pragma once
+
+#include "timer.h"
+#include "state_node.h"
+
+// NOTE: 对于攻击翻滚等受到时间影响的节点都添加了timer定时器
+class PlayerAttackState : public StateNode
+{
+public:
+	PlayerAttackState();
+	~PlayerAttackState() = default;
+
+	void on_enter() override;
+	void on_update(float delta) override;
+	void on_exit() override;
+
+private:
+	Timer timer;
+
+private:
+	void update_hit_box_position();
+};
+
+class PlayerDeadState : public StateNode
+{
+public:
+	PlayerDeadState();
+	~PlayerDeadState() = default;
+
+	void on_enter() override;
+	void on_update(float delta) override;
+
+private:
+	Timer timer;
+
+};
+
+class PlayerFallState : public StateNode
+{
+public:
+	PlayerFallState();
+	~PlayerFallState() = default;
+
+	void on_enter() override;
+	void on_update(float delta) override;
+
+};
+
+class PlayerIdleState : public StateNode
+{
+public:
+	PlayerIdleState();
+	~PlayerIdleState() = default;
+
+	void on_enter() override;
+	void on_update(float delta) override;
+
+};
+
+class PlayerJumpState : public StateNode
+{
+public:
+	PlayerJumpState();
+	~PlayerJumpState() = default;
+
+	void on_enter() override;
+	void on_update(float delta) override;
+
+};
+
+class PlayerRollState : public StateNode
+{
+public:
+	PlayerRollState();
+	~PlayerRollState() = default;
+
+	void on_enter() override;
+	void on_update(float delta) override;
+	void on_exit() override;
+
+private:
+	Timer timer;
+};
+
+class PlayerRunState : public StateNode
+{
+public:
+	PlayerRunState();
+	~PlayerRunState() = default;
+
+	void on_enter() override;
+	void on_update(float delta) override;
+	void on_exit() override;
+
+};
+
